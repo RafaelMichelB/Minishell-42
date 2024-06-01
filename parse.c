6034814149e3a1a0760cc1c5ebc108bcc5e57a7d
Rafael <rafmb30@gmail.com>
2024-06-01 21:52:50 +0200

    modified parse.c

--- parse.c
+++ parse.c
@@ -15,17 +15,84 @@ int is_in_str(char c, char *str)
     return (0);
 }
 
-t_list *parser(char *cmd)
+void    parse4(char *cmd, int *i, t_list **lst, int *start)
+{
+    if (cmd[*i] == '$')
+    {
+        *start = *i;
+        (*i)++;
+        while (ft_isalnum(cmd[*i]) || cmd[*i] == '_')
+            (*i)++;
+        ft_lstadd_back(lst, ft_lstnew(ft_substr(cmd, *start, *i - *start + 1)));
+        (*i)++;
+    }
+    else
+    {
+        *start = *i;
+        (*i)++;
+        while (is_in_str(cmd[*i], "'$<>| ") == 0 && cmd[*i] != '\0')
+            (*i)++;
+        ft_lstadd_back(lst, ft_lstnew(ft_substr(cmd, *start, *i - *start)));
+    }
+} // 17
+
+void    parse3(char *cmd, int *i, t_list **lst, int *start)
+{
+    if (cmd[*i] == 34)
+    {
+        (*i)++;
+        *start = *i;
+        while (cmd[*i] != 34)
+            (*i)++;
+        ft_lstadd_back(lst, ft_lstnew(ft_substr(cmd, *start, *i - *start)));
+        (*i)++;
+    }
+    else if (cmd[*i] == 39)
+    {
+        *start = *i;
+        (*i)++;
+        while (cmd[*i] != 39)
+            (*i)++;
+        ft_lstadd_back(lst, ft_lstnew(ft_substr(cmd, *start, *i - *start + 1)));
+        (*i)++;
+    }
+    else
+        parse4(cmd, i, lst, start);
+} // 20
+
+void    parse2(char *cmd, int *i, t_list **lst, int *start)
+{
+    if (cmd[*i] == '>')
+    {
+        if (cmd[*i + 1] == '>')
+        {
+            ft_lstadd_back(lst, ft_lstnew(ft_strdup(">>")));
+            (*i)++;
+        }
+        else
+            ft_lstadd_back(lst, ft_lstnew(ft_strdup(">")));
+        (*i)++;
+    }
+    else if (cmd[*i] == '|')
+    {
+        ft_lstadd_back(lst, ft_lstnew(ft_strdup("|")));
+        (*i)++;
+    }
+    else
+        parse3(cmd, i, lst, start);
+} //17
+
+t_list *parser(char *cmd, int i)
 {
     t_list  *lst;
-    int     i;
+    char    *var1;
+    char    *var2;
     int     start;
 
-    i = 0;
     lst = NULL;
     while (cmd[i] != '\0')
     {
-        if (cmd[i] == ' ' || cmd[i] == '\"')
+        if (cmd[i] == ' ')
             i++;
         else if (cmd[i] == '<')
         {
@@ -38,51 +105,11 @@ t_list *parser(char *cmd)
                 ft_lstadd_back(&lst, ft_lstnew(ft_strdup("<")));
             i++;
         }
-        else if (cmd[i] == '>')
-        {
-            if (cmd[i + 1] == '>')
-            {
-                ft_lstadd_back(&lst, ft_lstnew(ft_strdup(">>")));
-                i++;
-            }
-            else
-                ft_lstadd_back(&lst, ft_lstnew(ft_strdup(">")));
-            i++;
-        }
-        else if (cmd[i] == '|')
-        {
-            ft_lstadd_back(&lst, ft_lstnew(ft_strdup("|")));
-            i++;
-        }
-        else if (cmd[i] == '\'')
-        {
-            start = i;
-            i++;
-            while (cmd[i] != '\'')
-                i++;
-            ft_lstadd_back(&lst, ft_lstnew(ft_substr(cmd, start, i - start + 1)));
-            i++;
-        }
-        else if (cmd[i] == '$')
-        {
-            start = i;
-            i++;
-            while (ft_isalnum(cmd[i]) || cmd[i] == '_')
-                i++;
-            ft_lstadd_back(&lst, ft_lstnew(ft_substr(cmd, start, i - start + 1)));
-            i++;
-        }
         else
-        {
-            start = i;
-            i++;
-            while (is_in_str(cmd[i], "\"\'$<>| ") == 0 && cmd[i] != '\0')
-                i++;
-            ft_lstadd_back(&lst, ft_lstnew(ft_substr(cmd, start, i - start)));
-        }
+            parse2(cmd, &i, &lst, &start);
     }
     return (lst);
-}
+} // 25 
 
 #include <stdio.h>
 void    check_parsing(t_list **lst)
@@ -92,13 +119,11 @@ void    check_parsing(t_list **lst)
 
     flag = 0;
     cpy = *lst;
-   // printf("len :%ld\n", ft_strlen((*lst)->content));
     if (ft_strlen((*lst)->content) != 1 || ((char *)((*lst)->content))[0] != '<')
     {
         ft_lstadd_front(lst, ft_lstnew(ft_strdup("/dev/stdin")));
         ft_lstadd_front(lst, ft_lstnew(ft_strdup("<")));
     }
-  //  write(1, "B\n", 2);
     while (cpy != NULL)
     {
         if (((char *)(cpy->content))[0] == '|' )
@@ -113,83 +138,209 @@ void    check_parsing(t_list **lst)
         ft_lstadd_back(lst, ft_lstnew(ft_strdup("/dev/stdout")));
     }
     cpy = *lst;
-   // write(1, "A\n", 2);
     while (cpy->next->next != NULL)
         cpy = cpy->next;
-  //  printf("char: %s\n", (char *)(cpy->content));
     if ((ft_strlen(cpy->content) != 1 || ((char *)(cpy->content))[0] != '>') && \
     (ft_strlen(cpy->content) != 2 || (((char *)(cpy->content))[0] != '>' && ((char *)(cpy->content))[1] != '>')))
     {
         ft_lstadd_back(lst, ft_lstnew(ft_strdup(">")));
         ft_lstadd_back(lst, ft_lstnew(ft_strdup("/dev/stdout")));
     }
+} // 31
 
+static void free_tab(char **tab)
+{
+    int i;
+
+    i = 0;
+    while (tab[i])
+    {
+        free(tab[i]);
+        i++;
+    }
+    free(tab);
 }
 
-t_list *create_args(t_list *lst)
+char    *join_space(char *str_f, char *strtemp)
 {
+    char *temp;
+    char *to_ret;
+
+    temp = ft_strjoin(str_f, " ");
+    to_ret = ft_strjoin(temp, strtemp);
+    free(str_f);
+    free(strtemp);
+    return (to_ret);
+}
+
+char    *join_space_nof(char *str_f, char *strtemp)
+{
+    char *temp;
+    char *to_ret;
+
+    temp = ft_strjoin(str_f, " ");
+    to_ret = ft_strjoin(temp, strtemp);
+    free(str_f);
+    return (to_ret);
+}
+
+t_list *create_args(t_list *lst, int f)
+{
+    t_list *tmp;
     t_list *ret;
-    t_list *cpy;
-    char    *str[3];
+    char    **tab;
+    char    *str_temp;
+    char    *str[2];
+    char    *str_f;
+    int     flag;
+    int     n;
 
     str[0] = ft_calloc(1, sizeof(char));
     ret = NULL;
-    cpy = lst;
-    cpy = cpy->next;
-    ft_lstadd_back(&ret, ft_lstnew(ft_strdup(cpy->content)));
-    cpy = cpy->next;
-    while (((char *)(cpy->content))[0] != '>')
+    tmp = NULL;
+    while (lst != NULL)
     {
-        while (((char *)(cpy->content))[0] != '|' && ((char *)(cpy->content))[0] != '>')
+        str_f = ft_calloc(1, sizeof(char));
+        tab = ft_split(lst->content, ' ');
+        n = 0;
+        flag = 0;
+        while (tab[n])
         {
-            str[1] = ft_strjoin(str[0], cpy->content);
-            free(str[0]);
-            str[0] = ft_strjoin(str[1], " ");
-            free(str[1]);
-            cpy = cpy->next;
+            if (tab[n][0] == 39)
+            {
+                flag = 1;
+                str_temp = ft_strdup(tab[n]);
+            }
+            else if (tab[n][ft_strlen(tab[n]) - 1] == 39 && flag == 1)
+            {
+                flag = 0;
+                str_temp = ft_strdup(tab[n]);
+            }
+            else if (tab[n][0] == '$' && tab[n][1] == '?')
+                str_temp = ft_itoa(f);
+            else if (tab[n][0] == '$' && flag == 0)
+                str_temp = ft_strdup(getenv(&tab[n][1]));
+            else
+                str_temp = ft_strdup(tab[n]);
+            str_f = join_space(str_f, str_temp);
+            n++;
         }
-        ft_lstadd_back(&ret, ft_lstnew(ft_strdup(str[0])));
-       // ft_putstr_fd(str[0], 1);
-       // ft_putchar_fd('\n', 1);
-        free(str[0]);
-        str[0] = ft_calloc(1, sizeof(char));
-        if (((char *)(cpy->content))[0] != '>')
-            cpy = cpy->next;
+        ft_lstadd_back(&tmp, ft_lstnew(ft_strdup(str_f)));
+        free(str_f);
+        lst = lst->next;
     }
-    cpy = cpy->next;
-    ft_lstadd_back(&ret, ft_lstnew(ft_strdup(cpy->content)));
+    tmp = tmp->next;
+    ft_lstadd_back(&ret, ft_lstnew(ft_strdup(tmp->content)));
+    tmp = tmp->next;
+    while (((char *)(tmp->content))[1] != '>')
+    {
+        if (((char *)(tmp->content))[1] != '|' && ((char *)(tmp->content))[1] != '>')
+            str[0] = join_space_nof(str[0], &((char *)(tmp->content))[1]);
+        else
+        {
+            ft_lstadd_back(&ret, ft_lstnew(ft_strdup(str[0])));
+            free(str[0]);
+            str[0] = ft_calloc(1, sizeof(char));
+        }
+        tmp = tmp->next;
+    }
+    ft_lstadd_back(&ret, ft_lstnew(ft_strdup(str[0])));
+    tmp = tmp->next;
+    ft_lstadd_back(&ret, ft_lstnew(ft_strdup(tmp->content)));
     return (ret);
 }
 
 #include <fcntl.h>
-int main(int c, char **v, char **env)
+int main2(char *v, char **env, int *flag)
 {
     t_list *lst;
     t_list *cpy;
     t_list *ret;
     int fd[4];
     char **tab;
+    pid_t pid;
+    int status;
 
-    lst = parser(v[1]);
-    check_parsing(&lst);
-    cpy = lst;
-    /*while (cpy != NULL)
+    pid = fork();
+    if (pid == -1)
+        return (0);
+    if (pid == 0)
     {
-        printf("%s ", (char *)(cpy->content));
-        cpy = cpy->next;
-    }*/
-    //fd[0] = open("/dev/stdin", O_RDONLY);
-   // fd[1] = open("/dev/stdout", O_WRONLY);
-   // printf("Fds open: %d, %d\n", fd[0], fd[1]);
-    ret = create_args(lst);
-    tab = ft_calloc(ft_lstsize(ret) + 1, sizeof(char *));
-    tab[0] = "Start";
-    fd[2] = 1;
-    while (ret != NULL)
-    {
-        tab[fd[2]] = (char *)(ret->content);
-        ret = ret->next;
-        fd[2]++;
+        lst = parser(v, 0);
+        check_parsing(&lst);
+        cpy = lst;
+        while (cpy != NULL)
+            cpy = cpy->next;
+        ret = create_args(lst, *flag);
+        tab = ft_calloc(ft_lstsize(ret) + 1, sizeof(char *));
+        tab[0] = "Start";
+        fd[2] = 1;
+        while (ret != NULL)
+        {
+            tab[fd[2]] = &(((char *)(ret->content))[1]);
+            ret = ret->next;
+            fd[2]++;
+        }
+        pimain(fd[2], tab, env);
     }
-    pimain(fd[2], tab, env);
+    else
+    {
+        waitpid(pid, &status, 0);
+        *flag = status;
+    }
+}
+
+int main3(int c, char **v, char **env, int *flag)
+{
+    t_list *lst;
+    t_list *cpy;
+    t_list *ret;
+    int fd[4];
+    char **tab;
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid == -1)
+        return (0);
+    if (pid == 0)
+    {
+        lst = parser("echo 'Hello $?'", 0);
+        check_parsing(&lst);
+        cpy = lst;
+        while (cpy != NULL)
+            cpy = cpy->next;
+        ret = create_args(lst, *flag);
+        tab = ft_calloc(ft_lstsize(ret) + 1, sizeof(char *));
+        tab[0] = "Start";
+        fd[2] = 1;
+        while (ret != NULL)
+        {
+            tab[fd[2]] = &(((char *)(ret->content))[1]);
+            ret = ret->next;
+            fd[2]++;
+        }
+        pimain(fd[2], tab, env);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+        *flag = status;
+    }
+}
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(int c, char **v, char **env)
+{
+    int flag;
+    char *str;
+
+    flag = -1;
+    while (1)
+    {
+        str = readline(">> ");
+        main2(str, env, &flag);
+    }
 }
